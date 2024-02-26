da0a094731e0b356cd6e5ad33d8bf294154a630d
Yogesh Mokal <mokalyogesh113@gmail.com>
2024-02-26 18:55:16 +0530

    Updated guess-number/script.js

--- guess-number/script.js
+++ guess-number/script.js
@@ -6,13 +6,23 @@ const input = document.querySelector("input"),
 
 let attempts = 0;
 
+let randomNum = Math.floor(Math.random() * 10);
 function guessNumber() {
 
   if (checkButton.innerHTML == "Replay") {
-    window.navigation.reload()
+    // window.navigation.reload()
+    guess.innerHTML="";
+    checkButton.innerHTML = "check";
+    input.disabled = false;
+    input.value = "";
+    randomNum = Math.floor(Math.random() * 10);
+    attempts = 0;
+    total_attempts.innerHTML = attempts;
+    
+    return;   
+    
   }
 
-  let randomNum = Math.floor(Math.random() * 10);
   let inputValue = input.value;
 
 
@@ -39,12 +49,12 @@ function guessNumber() {
 
   if (inputValue == randomNum) {
     guess.innerHTML = "CONGRATULATIONS!..... <br> You Guessed it Right";
-    guess.style.color = "rgb(34,15  9,0)";
+    guess.style.color = "rgb(34,159,0)";
     input.disabled = true;
     checkButton.innerHTML = "Replay";
 
   } else if (inputValue != randomNum && inputValue < 10) {
-    guess.innerHTML = `Number was ${randomNum}  BAD LUCK :( <br> TRY AGAIN...`;
+    guess.innerHTML = ` BAD LUCK :( <br> TRY AGAIN...`;
     guess.style.color = "rgb(34,159,0)";
     input.focus();
   }
